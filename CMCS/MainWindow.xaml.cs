dde046f6d4465627270ec2ec3f168aff5a87c6e5
Ethaba <Ethaba@LAPTOP-JMB0UPB4>
2025-09-09 16:50:11 +0200

    Add project files.

---
+++ CMCS/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using CMCS.Views; // Import the Views folder
+
+namespace CMCS
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Load Lecturer Dashboard by default
+            MainContent.Content = new LecturerDashboard();
+
+            // Event handlers for navigation buttons
+            btnLecturer.Click += (s, e) => MainContent.Content = new LecturerDashboard();
+            btnClaimForm.Click += (s, e) => MainContent.Content = new ClaimForm();
+            btnCoordinator.Click += (s, e) => MainContent.Content = new CoordinatorDashboard();
+            btnManager.Click += (s, e) => MainContent.Content = new ManagerDashboard();
+        }
+    }
+}
